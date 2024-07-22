71224ab153046300efe6a92e8b055301094f1397
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2024-07-21 23:09:44 -0300

    Implementação e testes de ordenação no MAP

--- src/main/java/map/Pesquisa/EstoqueProdutos.java
+++ src/main/java/map/Pesquisa/EstoqueProdutos.java
@@ -50,6 +50,7 @@ public class EstoqueProdutos {
         estoque.adicionarProduto(1L, "Produto A", 10, 5.0);
         estoque.adicionarProduto(2L, "Produto B", 5, 10.0);
         estoque.adicionarProduto(3L, "Produto C", 2, 15.0);
+        estoque.adicionarProduto(8L, "Produto D", 2, 20.0);
 
         estoque.exibirProdutos();
 
